a1ad3d91903f2bb5621411741cc9a8ebde094c30
Ben Fulton <benjamin.fulton99@gmail.com>
2021-01-20 09:55:25 -0700

    Add stocking test for vendor

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -23,4 +23,18 @@ class VendorTest < Minitest::Test
 		assert_equal 0, @vendor.check_stock(@item1)
 	end
 
+	def test_it_can_add_stock
+		@vendor.stock(@item1, 30)
+
+		expected = {
+			@item1 => 30
+		}
+		assert_equal expected, @vendor.inventory
+		assert_equal 30, @vendor.check_stock(@item1)
+
+		@vendor.stock(@item1, 25)
+
+		assert_equal 55, @vendor.check_stock(@item1)
+	end
+
 end
