fb684bc222311b0118538cec4ec82b39f9a3d577
Ben Fulton <benjamin.fulton99@gmail.com>
2021-01-20 09:58:07 -0700

    Add vendor indexing test

--- test/market_test.rb
+++ test/market_test.rb
@@ -31,4 +31,18 @@ class MarketTest < Minitest::Test
 		assert_instance_of Market, @market
 	end
 
+	def test_it_has_things
+		assert_equal "South Pearl Street Farmers Market", @market.name
+		assert_equal [], @market.vendors
+	end
+
+	def test_it_can_add_vendors
+		@market.add_vendor(@vendor1)
+		@market.add_vendor(@vendor2)
+		@market.add_vendor(@vendor3)
+
+		assert_equal [@vendor1, @vendor2, @vendor3], @market.vendors
+	end
+
+
 end
